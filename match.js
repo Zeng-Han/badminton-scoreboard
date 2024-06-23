ae4267bb5770e89524785a8c97424e0ea21ada2e
Zeng-Han <1311014819@qq.com>
2024-06-23 17:26:39 +0800

    基本完成所有基础功能，美化了样式。

--- match.js
+++ match.js
@@ -5,6 +5,8 @@ let TEAM_1_PLAYER_1 = ''
 let TEAM_1_PLAYER_2 = ''
 let TEAM_2_PLAYER_1 = ''
 let TEAM_2_PLAYER_2 = ''
+let CURRENT_STATE = 0
+let LAST_SERVE_TEAM = '1'
 
 window.onload = function() {
     console.log('match.js loaded')
@@ -61,13 +63,13 @@ function addPoint(event) {
             if (MATCH_TYPE === 'singles') {
                 alert('比赛结束！' + PLAYER_1 + '获胜！')
             } else {
-                alert('比赛结束！' + TEAM_1_PLAYER_1 + '和' + TEAM_1_PLAYER_2 + '获胜！')
+                alert('比赛结束！' + TEAM_1_PLAYER_1 + '/' + TEAM_1_PLAYER_2 + '获胜！')
             }
         } else {
             if (MATCH_TYPE === 'singles') {
                 alert('比赛结束！' + PLAYER_2 + '获胜！')
             } else {
-                alert('比赛结束！' + TEAM_2_PLAYER_1 + '和' + TEAM_2_PLAYER_2 + '获胜！')
+                alert('比赛结束！' + TEAM_2_PLAYER_1 + '/' + TEAM_2_PLAYER_2 + '获胜！')
             }
         }
         window.history.back()
@@ -75,28 +77,49 @@ function addPoint(event) {
 
     // determine the players' positions
     if (MATCH_TYPE === 'singles') {
-        if (new_score % 2 === 0) {
-            console.log('serve at the right side')
-            let player_names_elements = document.getElementsByClassName('player')
-            for (let i = 0; i < player_names_elements.length; i++) {
-                player_names_elements[i].innerHTML = ''
-            }
-            let player_1_element = document.getElementById('team_1_player_right')
-            let player_2_element = document.getElementById('team_2_player_right')
-            player_1_element.innerHTML = PLAYER_1
-            player_2_element.innerHTML = PLAYER_2
+        if (new_score % 2 === 0 && CURRENT_STATE === 1 || new_score % 2 === 1 && CURRENT_STATE === 0) {
+            console.log('serve at the other side')
+            switchPlayers("1")
+            switchPlayers("2")
+            CURRENT_STATE = 1 - CURRENT_STATE  // switch the state
         } else {
-            console.log('serve at the left side')
-            let player_names_elements = document.getElementsByClassName('player')
-            for (let i = 0; i < player_names_elements.length; i++) {
-                player_names_elements[i].innerHTML = ''
-            }
-            let player_1_element = document.getElementById('team_1_player_left')
-            let player_2_element = document.getElementById('team_2_player_left')
-            player_1_element.innerHTML = PLAYER_1
-            player_2_element.innerHTML = PLAYER_2
+            console.log('serve at the same side')
         }
     } else {
-
+        // for doubles match
+        if (LAST_SERVE_TEAM === team_num) {
+            // the same team serves, switch the players, continue serving
+            switchPlayers(team_num)
+        }  // for different team serving, no need to switch players
+        LAST_SERVE_TEAM = team_num
+        CURRENT_STATE = new_score % 2
     }
+
+    // determine the serve sign
+    switchServeSign(team_num)
+}
+
+function switchPlayers(team_num) {
+    console.log('switch players: team ' + team_num)
+    let player_left = document.getElementById('team_' + team_num + '_player_left')
+    let player_right = document.getElementById('team_' + team_num + '_player_right')
+    let temp = player_left.innerHTML
+    player_left.innerHTML = player_right.innerHTML
+    player_right.innerHTML = temp
+}
+
+function switchServeSign(serve_team) {
+    console.log('switch serve sign: team ' + serve_team)
+    let serve_side = CURRENT_STATE === 0 ? 'right' : 'left'
+    let serve_side_opposite = CURRENT_STATE === 0 ? 'left' : 'right'
+    let serve_sign = document.getElementById('team_' + serve_team + '_sign_' + serve_side)
+    let serve_sign_opposite = document.getElementById('team_' + serve_team + '_sign_' + serve_side_opposite)
+    serve_sign.innerHTML = '<img src="serve.svg" alt=""/>'  // show the serve sign
+    serve_sign_opposite.innerHTML = ''  // hide the serve sign
+
+    let receive_team = serve_team === '1' ? '2' : '1'
+    let receive_sign = document.getElementById('team_' + receive_team + '_sign_' + serve_side)
+    let receive_sign_opposite = document.getElementById('team_' + receive_team + '_sign_' + serve_side_opposite)
+    receive_sign.innerHTML = '<img src="receive.svg" alt=""/>'  // hide the receive sign
+    receive_sign_opposite.innerHTML = ''  // show the receive sign
 }
